077dc6be355f88c0680fbc37cac1f18c8471ad9e
bmiww <toms.ozols5@gmail.com>
2022-03-09 18:19:03 +0100

    Added mandatory and optional field processing for google meet

--- disable-auto-gain-control.js
+++ disable-auto-gain-control.js
@@ -10,6 +10,7 @@
 // ==/UserScript==
 
 const doDisabling = () => {
+  const NEEDS_MANUAL_OPTIONAL = 'NEEDS_MANUAL_OPTIONAL';
   const TRAITS_TO_DISABLE = ['autoGainControl', 'googAutoGainControl', 'googAutoGainControl2'];
   const PATCHES = [
     [navigator, ['getUserMedia', 'mozGetUserMedia', 'webkitGetUserMedia']], // Legacy APIs
@@ -17,12 +18,24 @@ const doDisabling = () => {
     [MediaStreamTrack.prototype, ['applyConstraints']] // Sneaky API for post-updates
   ];
 
-  // TODO: Haven't covered some possible legacy options: constraint.optional/mandatory
-  // If in doubt - refer to the chrome extension
   const setConstraint = (constraintObj, name) => {
-    const config = constraintObj.advanced
-          && constraintObj.advanced.find(option => name in option)
+    const hasMandatoryKey = constraintObj.mandatory && name in constraintObj.mandatory;
+
+    let config =
+          hasMandatoryKey && constraintObj.mandatory
+          || constraintObj.optional && constraintObj.optional.find(option => name in option)
+          || constraintObj.mandatory && NEEDS_MANUAL_OPTIONAL
+          || constraintObj.advanced && constraintObj.advanced.find(option => name in option)
           || constraintObj;
+
+    if (config === NEEDS_MANUAL_OPTIONAL) {
+      if (!constraintObj.optional) {
+        constraintObj.optional = [];
+      }
+
+      constraintObj.optional.push({[name]: false});
+      return;
+    }
 
     config[name] = false;
   };
